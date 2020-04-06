d45594f6e7f151bb5b29cd1b60616ec3e38f8311
JunDamin <freedomgod@gmail.com>
2020-04-06 11:49:20 +0430

    #2.3. 관리자 페이지 수정

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (
+            "Basic Info",
+            {
+                "fields": ("author", "country", "article_date", "title", "subject_type", "article_field",)
+            },
+        ),
+        (
+            "Contents",
+            {
+                "fields": ("content", "article_source", "article_link",)
+            },
+        ),
+    )
+
+    list_display = ("author", "country", "article_date", "title", "subject_type", "article_field", "article_source", "article_link",
+        
+    )
+
+    list_filter = (
+        "subject_type",
+        "article_field",
+        "country",
+    )
+
+    raw_id_fields = ("author", )
+
+    search_fields = ["country", "title", "article_source"]
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from . import models
 # Register your models here.
+
+@admin.register(models.User)
+class CustomUserAdmin(admin.ModelAdmin):
+    
+    fieldsets = (("Organization",{"fields": ("office",) },),) + UserAdmin.fieldsets
+
+    list_filter = UserAdmin.list_filter + ("office",)
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "office",)
