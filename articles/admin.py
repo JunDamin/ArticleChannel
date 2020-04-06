9d2cde62fae7992dc53c3054f453521333d37f2b
JunDamin <freedomgod@gmail.com>
2020-04-06 22:07:45 +0430

    #6.1 excel import-export

--- articles/admin.py
+++ articles/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from import_export.admin import ImportExportMixin
 from . import models
 
 # Register your models here.
 
 @admin.register(models.Article)
-class ArticleAdmin(admin.ModelAdmin):
+class ArticleAdmin(ImportExportMixin, admin.ModelAdmin):
     fieldsets = (
         (
             "Basic Info",
--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = ["django_countries"]
+THIRD_PARTY_APPS = ["django_countries", "import_export"]
 
 PROJECT_APPS = ["core.apps.CoreConfig", "users.apps.UsersConfig", "articles.apps.ArticlesConfig"]
 
