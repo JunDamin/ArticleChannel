1dc23d443df2790b376c8ebfdf23ea7e910ecdb5
JunDamin <freedomgod@gmail.com>
2020-04-07 15:59:12 +0430

    $ 6.3 contries added

--- users/admin.py
+++ users/admin.py
@@ -5,22 +5,38 @@ from . import models
 # Register your models here.
 
 
+@admin.register(models.Country)
+class CustomUserAdmin(admin.ModelAdmin):
+
+    list_display = ("name",)
+
+    list_filter = ("name",)
+
+
 @admin.register(models.Department)
 class CustomUserAdmin(admin.ModelAdmin):
 
     fieldsets = (
         ("Organization", {"fields": ("name", "koica_code")},),
-        ("Countries", {"fields": ("workon_countries",)},),
-    )
-    list_filter = (
-        "name",
-        "workon_countries",
+        ("Countries", {"fields": ("countries",)},),
     )
 
     list_display = (
         "name",
-        "workon_countries",
+        "count_countries",
     )
+
+    list_filter = (
+        "name",
+        "countries",
+    )
+
+    filter_horizontal = ("countries",)
+
+    def count_countries(self, obj):
+        return obj.countries.count()
+
+    count_countries.short_description = "Country Count"
 
 
 @admin.register(models.User)
--- users/models.py
+++ users/models.py
@@ -6,17 +6,25 @@ from core import models as core_models
 
 # Create your models here.
 
+
+class Country(models.Model):
+    name = CountryField()
+
+    def __str__(self):
+        return self.name.name
+
+
 class Department(core_models.TimeStampedModel):
     name = models.CharField(max_length=255)
     koica_code = models.CharField(max_length=255)
-    workon_countries = CountryField(multiple=True, blank=True)
+    countries = models.ManyToManyField("Country", related_name="countries", blank=True)
 
     def __str__(self):
         return self.name
 
 
 class User(AbstractUser):
-    
+
     LOGIN_EMAIL = "email"
     LOGIN_GITHUB = "github"
     LOGIN_KAKAO = "kakao"
@@ -29,7 +37,13 @@ class User(AbstractUser):
 
     avatar = models.ImageField(upload_to="avatars", blank=True)
     bio = models.TextField(default="", blank=True)
-    department = models.ForeignKey('Department', related_name='department', on_delete=models.PROTECT, blank=True, null=True)
+    department = models.ForeignKey(
+        "Department",
+        related_name="department",
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+    )
 
     email_verified = models.BooleanField(default=False)
     email_secret = models.CharField(max_length=64, default="", blank=True)
@@ -37,10 +51,9 @@ class User(AbstractUser):
         max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
     )
 
-
     def get_absolute_url(self):
         return reverse("users:profile", kwargs={"pk": self.pk})
 
     def verify_email(self):
-        
+
         return
