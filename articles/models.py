2ef81e647f6f81c4bb1d5e1f1c436b13bd720bf3
JunDamin <freedomgod@gmail.com>
2020-04-08 10:20:59 +0430

    #7.4 Modified model choice Name

--- articles/models.py
+++ articles/models.py
@@ -6,18 +6,33 @@ from core import models as core_models
 
 # Create your models here.
 
+
 class Article(core_models.TimeStampedModel):
 
+    SUBJECT_TYPE_CHOICES = (
+        ("rc", "수원국"),
+        ("dn", "공여국"),
+        ("in", "국제기구"),
+        ("ko", "KOICA"),
+    )
+    SECTOR_CHOICES = (
+        ("edu", "교육"),
+        ("hea", "보건의료"),
+        ("gov", "공공행정"),
+        ("agr", "농림수산"),
+        ("tec", "기술환경에너지"),
+        ("emr", "긴급구호"),
+        ("etc", "기타"),
+    )
 
-    ARTICLE_FIELD_CHOICES = (("rc", "수원국"), ("dn", "공여국"), ("in", "국제기구"), ("ko", "KOICA"))
-    SUBJECT_TYPE_CHOICES = (("edu", "교육"), ("hea", "보건의료"), ("gov", "공공행정"), ("agr", "농림수산"), ("tec", "기술환경에너지"), ("emr", "긴급구호"), ("etc", "기타"))
-
-    author = models.ForeignKey('users.User', related_name='article', on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        "users.User", related_name="article", on_delete=models.CASCADE
+    )
     country = CountryField()
     article_date = models.DateField()
     title = models.CharField(max_length=255)
     subject_type = models.CharField(max_length=16, choices=SUBJECT_TYPE_CHOICES)
-    sector = models.CharField(max_length=16, choices=ARTICLE_FIELD_CHOICES)
+    sector = models.CharField(max_length=16, choices=SECTOR_CHOICES)
     content = models.TextField()
     article_source = models.CharField(max_length=255, blank=True)
     article_link = models.TextField(blank=True)
